4371b627947525da1af01c86c52d45e3a842932f
兩河流域 <teariver0220@gmail.com>
2025-12-23 18:55:37 +0800

    versioning, gitignore

--- DownloadIt/app/build.gradle.kts
+++ DownloadIt/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     applicationId = "com.rayliu0712.saveit"
     minSdk = 29
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "2.0.0-alpha"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
